83ab8f5f4879c59cdf652f30147cede615a588aa
qdwang <wangqidong@gmail.com>
2022-10-14 22:10:35 +0800

    reduce O4 to O3

--- build-wasm.sh
+++ build-wasm.sh
@@ -3,5 +3,5 @@ echo "generating wasm bindgen"
 wasm-bindgen --no-typescript --target web --out-dir pkg ./target/wasm32-unknown-unknown/release/quickraw.wasm
 cd pkg
 echo "optimizing wasm"
-wasm-opt -O4 quickraw_bg.wasm -o quickraw_bg.wasm
+wasm-opt -O3 quickraw_bg.wasm -o quickraw_bg.wasm
 echo "* wasm and js file are built under the ./pkg folder"
