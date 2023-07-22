35dd33e2498c13c6aa782a0fc16a153fe3e8adf6
qdwang <wangqidong@gmail.com>
2023-07-22 12:06:48 +0800

    add desc

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
+#![allow(dead_code)]
+
+//! Process steps:
+//! 1. Create EXIF parsing rule.
+//! 2. Extract needed EXIF info from the RAW file.
+//! 3. Decode RAW image data to `Vec<u16>`.
+//! 4. Do preprocesses like BlackLevel substraction and color level scaling up, etc.
+//! 5. Do demosaicing.
+//! 6. Fix WhiteBalance. (Optional)
+//! 7. Convert Colorspace to the target one. (Optional)
+//! 
+//! 
 erreport::gen_report_code!();
 
+pub(crate) mod parse;
 pub(crate) mod tool;
 pub mod data;
 
