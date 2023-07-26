556517626f49aac48821f4e5abcc907409a0dfad
qdwang <wangqidong@gmail.com>
2023-07-26 17:25:04 +0800

    add dump argument to arw example

--- examples/arw.rs
+++ examples/arw.rs
@@ -1,15 +1,21 @@
-use std::time::Instant;
+use std::env;
 
 fn main() {
-    let t = Instant::now();
-    let (image_u16, w, h) = quickraw::extract_image("examples/samples/sample1.ARW").unwrap();
-    println!("time elapsed: {:?}", Instant::now() - t);
+    let args = env::args().collect::<Box<_>>();
+    let args = args.into_iter().map(|x| x.as_str()).collect::<Box<_>>();
 
-    let image_beu8: Vec<_> = image_u16
-        .into_iter()
-        .flat_map(|v| v.to_be_bytes())
-        .collect();
-    let mut ppm_header = format!("P6 {w} {h} 65535\n").as_bytes().to_vec();
-    ppm_header.extend(image_beu8);
-    // std::fs::write("sample.ppm", ppm_header).unwrap();
+    let (image_u16, w, h) = quickraw::extract_image("examples/samples/sample1.ARW").unwrap();
+
+    match &args[..] {
+        [_, "dump"] => {
+            let image_beu8: Box<_> = image_u16
+                .into_iter()
+                .flat_map(|v| v.to_be_bytes())
+                .collect();
+            let mut ppm_header = format!("P6 {w} {h} 65535\n").as_bytes().to_vec();
+            ppm_header.extend(&image_beu8[..]);
+            std::fs::write("dump.ppm", ppm_header).unwrap();
+        }
+        _ => {}
+    }
 }
