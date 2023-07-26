72c4d29cf01f9ee1ff90e77c6e0ece57a3a4f718
qdwang <wangqidong@gmail.com>
2023-07-26 16:48:16 +0800

    remove unsafe

--- src/demosaicing/mod.rs
+++ src/demosaicing/mod.rs
@@ -66,21 +66,13 @@ fn get_pixel_type(i: usize, w: usize, h: usize) -> PixelType {
     }
 }
 
-trait FastGet {
-    fn fast_get(&self, i: usize) -> u16;
-}
-impl FastGet for &[u16] {
-    fn fast_get(&self, i: usize) -> u16 {
-        unsafe { *self.get_unchecked(i) }
-    }
-}
 
 #[inline(always)]
 fn avg_tb_lr(image: &[u16], i: usize, w: usize) -> (u16, u16) {
-    let a = image.fast_get(i - w) as u32;
-    let b = image.fast_get(i + w) as u32;
-    let c = image.fast_get(i - 1) as u32;
-    let d = image.fast_get(i + 1) as u32;
+    let a = image[i - w] as u32;
+    let b = image[i + w] as u32;
+    let c = image[i - 1] as u32;
+    let d = image[i + 1] as u32;
 
     let x = (a + b) / 2;
     let y = (c + d) / 2;
@@ -92,15 +84,15 @@ fn avg_corner_4(image: &[u16], i: usize, w: usize) -> (u16, u16) {
     let top: usize = i - w;
     let bottom: usize = i + w;
 
-    let a = image.fast_get(top - 1) as u32;
-    let b = image.fast_get(top + 1) as u32;
-    let c = image.fast_get(bottom - 1) as u32;
-    let d = image.fast_get(bottom + 1) as u32;
+    let a = image[top - 1] as u32;
+    let b = image[top + 1] as u32;
+    let c = image[bottom - 1] as u32;
+    let d = image[bottom + 1] as u32;
 
-    let e = image.fast_get(top) as u32;
-    let f = image.fast_get(bottom) as u32;
-    let g = image.fast_get(i - 1) as u32;
-    let h = image.fast_get(i + 1) as u32;
+    let e = image[top] as u32;
+    let f = image[bottom] as u32;
+    let g = image[i - 1] as u32;
+    let h = image[i + 1] as u32;
 
     let x = (a + b + c + d) / 4;
     let y = (e + f + g + h) / 4;
