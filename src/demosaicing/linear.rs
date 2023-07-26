9c3bb5839effc800db20b6b4a5274d4483584313
qdwang <wangqidong@gmail.com>
2023-07-26 20:33:47 +0800

    handle edge pixels

--- src/demosaicing/linear.rs
+++ src/demosaicing/linear.rs
@@ -3,6 +3,14 @@ use super::*;
 #[inline(always)]
 pub(crate) fn rggb(i: usize, w: usize, h: usize, v: u16, image: &[u16]) -> [u16; 3] {
     match get_pixel_type(i, w, h) {
+        PixelType::TopLeft | PixelType::TopEven => [v, image[i + 1], image[i + w + 1]],
+        PixelType::TopRight | PixelType::TopOdd => [image[i - 1], v, image[i + w]],
+        PixelType::BottomLeft | PixelType::BottomEven => [image[i - w], v, image[i + 1]],
+        PixelType::BottomRight | PixelType::BottomOdd => [image[i - w - 1], image[i - 1], v],
+        PixelType::LeftEven => [v, image[i + 1], image[i + w + 1]],
+        PixelType::LeftOdd => [image[i + w], v, image[i + 1]],
+        PixelType::RightEven => [image[i - 1], v, image[i + w]],
+        PixelType::RightOdd => [image[i + w - 1], image[i - 1], v],
         PixelType::Center0 => {
             let (a, b) = avg_corner_4(image, i, w);
             [v, b, a]
@@ -19,6 +27,5 @@ pub(crate) fn rggb(i: usize, w: usize, h: usize, v: u16, image: &[u16]) -> [u16;
             let (a, b) = avg_corner_4(image, i, w);
             [a, b, v]
         }
-        _ => [v, v, v]
     }
 }
